d9b846400ebe96b1cb7b0b29f3fdf8e52d793df6
Gatlin Cheyenne Johnson <gatlin@niltag.net>
2021-04-09 21:12:30 -0500

    completely superficial code cleanup

--- src/ceskm.ts
+++ src/ceskm.ts
@@ -59,15 +59,15 @@ export type State<T> = {
 };
 
 /* The CESKM virtual machine */
-export class CESKM<Result = never> {
-  protected result: Value<Result> | null = null;
+export class CESKM<Base = never> {
+  protected result: Value<Base> | null = null;
   protected gensym_count: number = 0;
 
   constructor (
     protected readonly control: Cbpv
   ) {}
 
-  protected make_initial_state(): State<Result> {
+  protected make_initial_state(): State<Base> {
     return {
       control: this.control,
       environment: [],
@@ -77,14 +77,14 @@ export class CESKM<Result = never> {
 
   /**
    * @method run
-   * @returns { Value<Result> } The result value of the control expression.
+   * @returns { Value<Base> } The result value of the control expression.
    */
-  public run(): Value<Result> {
-    let st: State<Result> = this.make_initial_state();
+  public run(): Value<Base> {
+    let st: State<Base> = this.make_initial_state();
     while (!this.result) {
       let res = this.step(clone(st));
       if (!this.result) {
-        st = <State<Result>>res; }}
+        st = <State<Base>>res; }}
     return this.result; }
 
   /**
@@ -96,23 +96,23 @@ export class CESKM<Result = never> {
 
   /**
    * @method literal
-   * @returns { Value<Result> } A value representation of the syntactic literal.
+   * @returns { Value<Base> } A value representation of the syntactic literal.
    * Sub-classes will need to override this method if they change the type T.
    */
-  protected literal(v: any): Value<Result> {
+  protected literal(v: any): Value<Base> {
     return closure(cbpv_lit(v), []); }
 
   /**
    * @method positive
    * @param { Cbpv } expr The positive expression we are evaluating.
    * @param { Env } env
-   * @param { Store<Result> } store
-   * @returns { Value<Result> } Resulthe resulting value from a positive term.
+   * @param { Store<Base> } store
+   * @returns { Value<Base> } The resulting value from a positive term.
    * @throws if the expression isn't positive.
    * @remarks Runs in a loop so that arbitrarily-nested suspensions can be
    * evaluated in one call.
    */
-  private positive(expr: Cbpv, env: Env, store: Store<Result>): Value<Result> {
+  private positive(expr: Cbpv, env: Env, store: Store<Base>): Value<Base> {
     let finished: boolean = false;
     while (!finished) {
       switch (expr.tag) {
@@ -144,8 +144,8 @@ export class CESKM<Result = never> {
 
   /**
    * @method step
-   * @param {State<Result>} state
-   * @returns { Value<Result> | State<Result> } Returns a `State` in the event that there is
+   * @param {State<Base>} state
+   * @returns { Value<Base> | State<Base> } Returns a `State` in the event that there is
    * more work to be done, but otherwise it returns the final result `Value`.
    * @remarks Advances the machine forward one step. Some terms
    * (namely, `AppA`, `LetA`, and `LetrecA`) do not constitute a complete step
@@ -154,15 +154,9 @@ export class CESKM<Result = never> {
    * is for; this function is still guaranteed™ to terminate for well-formed
    * inputs.
    */
-  protected step(state: State<Result>): Value<Result> | State<Result> {
+  protected step(state: State<Base>): Value<Base> | State<Base> {
     let finished: boolean = false;
-    let {
-      control,
-      environment,
-      store,
-      kontinuation,
-      meta
-    } = state;
+    let { control, environment, store, kontinuation, meta } = state;
 
     while (!finished) {
       switch (control.tag) {
@@ -186,30 +180,20 @@ export class CESKM<Result = never> {
           break; }
         case "cbpv_shift": {
           let addr: string = this.gensym();
-          let cc: Kont<Result> = kontinuation;
+          let cc: Kont<Base> = kontinuation;
           let frame: Frame = {};
           frame[control.karg] = addr;
           env_push_frame(frame, environment);
           control = control.body;
           store[addr] = continuation(cc);
           kontinuation = topk();
-          return {
-            control,
-            environment,
-            store,
-            kontinuation,
-            meta }; }
+          return { control, environment, store, kontinuation, meta }; }
         case "cbpv_reset": {
-          let cc: Kont<Result> = kontinuation;
+          let cc: Kont<Base> = kontinuation;
           control = control.exp;
           kontinuation = topk();
           meta.unshift(cc);
-          return {
-            control,
-            environment,
-            store,
-            kontinuation,
-            meta }; }
+          return { control, environment, store, kontinuation, meta }; }
         case "cbpv_if": {
           let cv = this.positive(control.c, environment, store);
           if (! ("v" in cv))
@@ -217,23 +201,14 @@ export class CESKM<Result = never> {
           if ("boolean" !== typeof cv.v)
             { throw new Error("`if` conditional must be boolean"); }
           control = cv.v ? control.t : control.e;
-          return {
-            control,
-            environment,
-            store,
-            kontinuation,
-            meta }; }
+          return { control, environment, store, kontinuation, meta }; }
         case "cbpv_resume": {
           let val = this.positive(control.v, environment, store);
           if ("_exp" in val) {
             control = val._exp;
             environment = val._env;
-            return {
-              control,
-              environment,
-              store,
-              kontinuation,
-              meta }; }
+            return { control, environment, store, kontinuation, meta };
+          }
           else {
             return this.continue(val, kontinuation, store, meta); }}
         case "cbpv_abstract": {
@@ -246,12 +221,7 @@ export class CESKM<Result = never> {
             control = control.body;
             env_push_frame(frame, environment);
             kontinuation = kontinuation._kont;
-            return {
-              control,
-              environment,
-              store,
-              kontinuation,
-              meta }; }
+            return { control, environment, store, kontinuation, meta }; }
           throw new Error('invalid continuation for function'); }
         default: finished = true; } }
     return this.continue(
@@ -262,29 +232,29 @@ export class CESKM<Result = never> {
 
   /**
    * @method continue
-   * @param { Value<Result> } val
-   * @param { Kont<Result> } kontinuation
-   * @param { Store<Result> } store
-   * @param { Kont<Result>[] } meta
-   * @returns { Value<Result> | State<Result> } A result `Value` or another `State` if there is
+   * @param { Value<Base> } val
+   * @param { Kont<Base> } kontinuation
+   * @param { Store<Base> } store
+   * @param { Kont<Base>[] } meta
+   * @returns { Value<Base> | State<Base> } A result `Value` or another `State` if there is
    * more work to be done.
    * @remarks This method tries to apply the current continuation to a value.
    */
   private continue(
-    val: Value<Result>,
-    kontinuation: Kont<Result>,
-    store: Store<Result>,
-    meta: Kont<Result>[]
-  ): Value<Result> | State<Result> {
+    val: Value<Base>,
+    kontinuation: Kont<Base>,
+    store: Store<Base>,
+    meta: Kont<Base>[]
+  ): Value<Base> | State<Base> {
     let finished: boolean = false;
     while (!finished) {
       if ("_args" in kontinuation) {
         let { _args, _kont } = kontinuation;
-        let actual_val: Value<Result> = _args[0];
-        let next_k: Kont<Result> = _kont;
+        let actual_val: Value<Base> = _args[0];
+        let next_k: Kont<Base> = _kont;
         meta.unshift(next_k);
         if (! ("_kont" in val))
-          { throw new Error(`boo: ${JSON.stringify(val)}`); }
+          { throw new Error(`expected continuation: ${JSON.stringify(val)}`); }
         else
           { kontinuation = val._kont; }
         val = actual_val; }
@@ -306,21 +276,21 @@ export class CESKM<Result = never> {
           this.result = val;
           return val; }
         else {
-          let k: Kont<Result> = meta.shift()!;
+          let k: Kont<Base> = meta.shift()!;
           kontinuation = k; } } }
     throw new Error("Invalid continuation."); }
 
   /**
    * @method primop
    * @param {string} op_sym the symbol for the primitive operator.
-   * @param {Array<Value<Result>>} args the values passed to the operator.
-   * @returns {Value<Result>}
+   * @param {Array<Value<Base>>} args the values passed to the operator.
+   * @returns {Value<Base>}
    * @remarks This method is protected expressly so that sub-classes may define
    * custom primitive operators (indeed that is a primary motivation for
    * sub-classes). Those provided here are chosen because in all likelihood
    * sub-classes will still find them useful.
    */
-  protected primop(op_sym: string, args: Value<Result>[]): Value<Result> {
+  protected primop(op_sym: string, args: Value<Base>[]): Value<Base> {
     let s = "";
     for (let arg of args) { s += ` ${ "v" in arg ? typeof arg.v :  "unknown" }`; }
     throw new Error(`bad prim op or arguments: ${op_sym} - ${s}`); }}
